dd85db0c2fd6c1763fde749a916e943c25655788
chamsou-Br <jc_berkane@esi.dz>
2023-02-27 16:56:29 +0100

    init

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 // Import the express in typescript file
 import express , {Request,Response,Application} from 'express';
+
  
 // Initialize the express engine
 const app:Application = express();
- 
+
 // Take a port 3000 for running server.
 // eslint-disable-next-line @typescript-eslint/ban-types
 const port : Number = 3000;
