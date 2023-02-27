20d6093a0b8f9b355513f6c1c090def9ffa67242
chamsou-Br <jc_berkane@esi.dz>
2023-02-27 16:34:44 +0100

    init

---
+++ index.ts
@@ -0,0 +1,22 @@
+// Import the express in typescript file
+import express , {Request,Response,Application} from 'express';
+ 
+// Initialize the express engine
+const app:Application = express();
+ 
+// Take a port 3000 for running server.
+// eslint-disable-next-line @typescript-eslint/ban-types
+const port : Number = 3000;
+ 
+// Handling '/' Request
+app.get('/', (_req : Request, _res :Response) => {
+    _res.send("TypeScript With Express");
+});
+ 
+// Server setup
+app.listen(port, () => {
+    console.log(`TypeScript with Express
+         http://localhost:${port}/`);
+
+});
+
