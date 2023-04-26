dc1308dc1898549b8cee8e31f8b43d17f239a643
chamsou-Br <jc_berkane@esi.dz>
2023-04-26 20:38:36 +0100

    change to render

--- src/controllers/card/cardController.ts
+++ src/controllers/card/cardController.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { BadRequestError, ForbiddenError, InternalError, NotFoundError } from "../../handler/apiError";
-import { SuccessMsgResponse, SuccessResponse } from "../../handler/apiResponse";
+import { SuccessMsgResponse, SuccessResponse } from "../../handler/ApiResponse";
 import asyncHandler from "../../handler/asyncHandler";
 import { onAddBeverageHandler, onDeleteBeverageHandler, onGetAllBeverageHandler, onGetBeverageHandler, onGetBeveragesOfMachineHandler, onUpdateBeverageHandler } from "../../services/beverageService";
 import { onGetMachineBydistUIDHandler } from "../../services/machinService";
