e718f22fe21f026b6877980d8f5c63d0fe12f243
chamsou-Br <jc_berkane@esi.dz>
2023-05-07 09:38:03 +0100

    credentials

--- index.ts
+++ index.ts
@@ -33,8 +33,6 @@ import compression from 'compression';
 import cors, { CorsOptions } from 'cors';
 import cookieParser from 'cookie-parser';
 import { createServer } from 'http';
-import {Server} from 'socket.io'
-import { Module } from 'module';
 import { ioMiddleware, onConnectionHandler } from './src/controllers/socketio/socketioController';
 
 import advrtRouter from './src/routers/advertisementRouter';
@@ -64,7 +62,7 @@ io.use(ioMiddleware)
 io.on("connection", onConnectionHandler)
 
 app.use(compression())
-app.use(cors());
+app.use(cors({credentials  : true}));
 
 //to parse everything to json
 app.use(cookieParser());
@@ -86,22 +84,20 @@ httpServer.listen(port, () => {
       console.log("first")
       res.send("Welcom with us :)")
    })
+
 // Server setup
 // app.listen(port, () => {
 //    console.log(`TypeScript with Express
 //    http://localhost:${port}/`);
 // });
+
 app.use("/order", orderRouter);
 app.use('/', authRouter);
-
 app.use('/user', userRouter);
-
 app.use('/machine', machinRouter);
-
 app.use("/beverage",baverageRouter);
 app.use("/advertisement",advrtRouter);
 app.use("/advertiser",advertiserRouter)
-
 app.use("/card",cardRouter)
 
 app.use("/profile",profileRouter)
