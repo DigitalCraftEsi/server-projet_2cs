5ab9a4d36cc5abe2b026c859751ee7ac0c60f246
chamsou-Br <jc_berkane@esi.dz>
2023-05-10 14:00:52 +0100

    add domain

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -48,7 +48,7 @@ export const createCookie = (res: Response, token:string, name:string, expiresIn
         httpOnly: true,
         expires: new Date(Date.now() + expiresIn * 24 * 60 * 60 * 1000),
         secure: process.env.NODE_ENV === "production" ? true : false,
-        domain : process.env.FRONTEND_DOMAIN
+        domain : ""
     };
     
     return res.cookie(name, token,cookieOption );
