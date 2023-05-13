b17cf20664e29b5570b0e3662974b027fc4daa71
Rechem <ir_chadouli@esi.dz>
2023-05-13 20:57:12 +0100

    now sending token in response

--- src/routers/authRouter.ts
+++ src/routers/authRouter.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, loginConsumer, signUpConsumer } from "../controllers/auth/authController";
+import { login, loginConsumer, signUpConsumer, refreshToken } from "../controllers/auth/authController";
 
 const authRouter = express.Router();
 
 authRouter.post("/login", login);
+authRouter.post("/refreshToken", refreshToken);
 authRouter.post("/loginconsumer", loginConsumer);
 authRouter.post("/signupconsumer", signUpConsumer);
 
