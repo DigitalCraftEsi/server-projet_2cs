82914740e76a76de1a765c62af9c29181b2ac1a1
chamsou-Br <jc_berkane@esi.dz>
2023-06-19 01:02:11 +0100

    some change

---
+++ prisma/migrations/20230618235848_/migration.sql
@@ -0,0 +1,110 @@
+-- DropIndex
+DROP INDEX `ac_idClient_fkey` ON `ac`;
+
+-- DropIndex
+DROP INDEX `adm_idClient_fkey` ON `adm`;
+
+-- DropIndex
+DROP INDEX `annoncepublicitaire_idAnnonceur_fkey` ON `annoncepublicitaire`;
+
+-- DropIndex
+DROP INDEX `annoncepublicitaire_idBoisson_fkey` ON `annoncepublicitaire`;
+
+-- DropIndex
+DROP INDEX `annoncepublicitaire_idDistributeur_fkey` ON `annoncepublicitaire`;
+
+-- DropIndex
+DROP INDEX `idTache` ON `anomalie`;
+
+-- DropIndex
+DROP INDEX `boisson_idDistributeur_fkey` ON `boisson`;
+
+-- DropIndex
+DROP INDEX `boissoncommande_idCommande_fkey` ON `boissoncommande`;
+
+-- DropIndex
+DROP INDEX `card_idConsommateur_fkey` ON `card`;
+
+-- DropIndex
+DROP INDEX `commande_idConsommateur_fkey` ON `commande`;
+
+-- DropIndex
+DROP INDEX `commande_idDistributeur_fkey` ON `commande`;
+
+-- DropIndex
+DROP INDEX `decideur_idClient_fkey` ON `decideur`;
+
+-- DropIndex
+DROP INDEX `distributeur_idAM_fkey` ON `distributeur`;
+
+-- DropIndex
+DROP INDEX `idTache` ON `panne`;
+
+-- AlterTable
+ALTER TABLE `consommateur` ADD COLUMN `picture` VARCHAR(200) NULL;
+
+-- AlterTable
+ALTER TABLE `panne` ADD COLUMN `qte` INTEGER NULL;
+
+-- AlterTable
+ALTER TABLE `tache` ADD COLUMN `type` VARCHAR(50) NULL;
+
+-- AddForeignKey
+ALTER TABLE `actionsuspecte` ADD CONSTRAINT `actionsuspecte_idDistributeur_fkey` FOREIGN KEY (`idDistributeur`) REFERENCES `distributeur`(`idDistributeur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `adm` ADD CONSTRAINT `adm_idClient_fkey` FOREIGN KEY (`idClient`) REFERENCES `client`(`idClient`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `annoncepublicitaire` ADD CONSTRAINT `annoncepublicitaire_idBoisson_fkey` FOREIGN KEY (`idBoisson`) REFERENCES `boisson`(`idBoisson`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `annoncepublicitaire` ADD CONSTRAINT `annoncepublicitaire_idDistributeur_fkey` FOREIGN KEY (`idDistributeur`) REFERENCES `distributeur`(`idDistributeur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `annoncepublicitaire` ADD CONSTRAINT `annoncepublicitaire_idAnnonceur_fkey` FOREIGN KEY (`idAnnonceur`) REFERENCES `annonceur`(`idAnnonceur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `anomalie` ADD CONSTRAINT `anomalie_idTache_fkey` FOREIGN KEY (`idTache`) REFERENCES `tache`(`idTache`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `boisson` ADD CONSTRAINT `boisson_idDistributeur_fkey` FOREIGN KEY (`idDistributeur`) REFERENCES `distributeur`(`idDistributeur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `boissoncommande` ADD CONSTRAINT `boissoncommande_idBoisson_fkey` FOREIGN KEY (`idBoisson`) REFERENCES `boisson`(`idBoisson`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `boissoncommande` ADD CONSTRAINT `boissoncommande_idCommande_fkey` FOREIGN KEY (`idCommande`) REFERENCES `commande`(`idCommande`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commande` ADD CONSTRAINT `commande_idConsommateur_fkey` FOREIGN KEY (`idConsommateur`) REFERENCES `consommateur`(`idConsommateur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `commande` ADD CONSTRAINT `commande_idDistributeur_fkey` FOREIGN KEY (`idDistributeur`) REFERENCES `distributeur`(`idDistributeur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `card` ADD CONSTRAINT `card_idConsommateur_fkey` FOREIGN KEY (`idConsommateur`) REFERENCES `consommateur`(`idConsommateur`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `decideur` ADD CONSTRAINT `decideur_idClient_fkey` FOREIGN KEY (`idClient`) REFERENCES `client`(`idClient`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `distributeur` ADD CONSTRAINT `distributeur_idAM_fkey` FOREIGN KEY (`idAM`) REFERENCES `am`(`idAM`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `distributeur` ADD CONSTRAINT `distributeur_idClient_fkey` FOREIGN KEY (`idClient`) REFERENCES `client`(`idClient`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `panne` ADD CONSTRAINT `panne_idTache_fkey` FOREIGN KEY (`idTache`) REFERENCES `tache`(`idTache`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `reclamation` ADD CONSTRAINT `reclamation_idCommande_fkey` FOREIGN KEY (`idCommande`) REFERENCES `commande`(`idCommande`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `reponse` ADD CONSTRAINT `reponse_idReclamation_fkey` FOREIGN KEY (`idReclamation`) REFERENCES `reclamation`(`idReclamation`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `ac` ADD CONSTRAINT `ac_idClient_fkey` FOREIGN KEY (`idClient`) REFERENCES `client`(`idClient`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `am` ADD CONSTRAINT `am_idClient_fkey` FOREIGN KEY (`idClient`) REFERENCES `client`(`idClient`) ON DELETE RESTRICT ON UPDATE CASCADE;
