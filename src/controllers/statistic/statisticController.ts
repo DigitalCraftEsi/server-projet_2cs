1a4d8eab7a29e4707963fdc1fd79d98c7dc8ffe5
chamsou-Br <jc_berkane@esi.dz>
2023-06-20 16:09:44 +0100

    add statistic

--- src/controllers/statistic/statisticController.ts
+++ src/controllers/statistic/statisticController.ts
@@ -25,6 +25,8 @@ export const getStatictic = asyncHandler(
 
 
         const orders = await onGetOrderOfClientHandler(user.clientId)
+        const orderSucess = (orders as any[]).filter(it =>it.status == commandeStatus.terminee)
+        const orderfailed = (orders as any[]).filter(it =>it.status == commandeStatus.echouee)
 
         // Group items by address
         const groupedData1 = (orders as any[]).reduce((acc, item) => {
@@ -45,13 +47,15 @@ export const getStatictic = asyncHandler(
         const taux = data1.map(it=>it.taux);
 
 
+        // statistic 2 MONTH
+
         let orderSucessMonth : number[] = [0,0,0,0,0,0,0,0,0,0,0,0];
         let orderFailedMonth : number[] = [0,0,0,0,0,0,0,0,0,0,0,0];
           // Set count to 0 for months with no elements
           for (let month = 0; month < 12; month++) {
                 orderSucessMonth[month] = 0;
         }
-        (orders as any[]).filter(it =>it.status == commandeStatus.terminee).forEach(item => {
+        orderSucess.forEach(item => {
             const date = new Date(item.dateCommande);
             const month = date.getMonth();
             orderSucessMonth[month] ++;
@@ -59,13 +63,83 @@ export const getStatictic = asyncHandler(
         for (let month = 0; month < 12; month++) {
             orderFailedMonth[month] = 0;
         }
-        (orders as any[]).filter(it =>it.status == commandeStatus.echouee).forEach(item => {
+        orderfailed.forEach(item => {
             const date = new Date(item.dateCommande);
             const month = date.getMonth();
             orderFailedMonth[month] ++;
         })
 
+        // statistic 2 Year 
+        const yearsSucess = [];
+        const countByYearSucess = [];
 
-        new SuccessResponse("sucess",{statistic1 : {address , taux} , statistic2_Month : {success : orderSucessMonth , failed : orderFailedMonth} }).send(res);
+        for (const item of orderSucess) {
+            const date = new Date(item.dateCommande);
+            const year = date.getFullYear();
+        
+            if (!yearsSucess.includes(year)) {
+                yearsSucess.push(year);
+                countByYearSucess.push(1);
+            } else {
+            const index = yearsSucess.indexOf(year);
+            countByYearSucess[index]++;
+            }
+        }
+
+        const yearsFailed = [];
+        const countByYearFailed = [];
+
+        for (const item of orderfailed) {
+            const date = new Date(item.dateCommande);
+            const year = date.getFullYear();
+        
+            if (!yearsFailed.includes(year)) {
+                yearsFailed.push(year);
+                countByYearFailed.push(1);
+            } else {
+            const index = yearsFailed.indexOf(year);
+            countByYearFailed[index]++;
+            }
+        }
+
+        // statistic 2 WEAK
+
+        const currentDate = new Date();
+        const startDate = new Date();
+        startDate.setDate(currentDate.getDate() - 28);
+        const itemCountsByWeekSucess = [];
+        const itemCountsByWeekfailed = [];
+        
+        for (let i = 0; i < 4; i++) {
+          const weekStart = new Date(startDate);
+          weekStart.setDate(weekStart.getDate() + i * 7);
+          const weekEnd = new Date(weekStart);
+          weekEnd.setDate(weekEnd.getDate() + 6);
+          const commandsForWeek = orderSucess.filter((command) => {
+            const commandDate = new Date(command.dateCommande);
+            return commandDate >= weekStart && commandDate <= weekEnd;
+          });
+        
+          const itemCount = commandsForWeek.length;
+          itemCountsByWeekSucess.push(itemCount);
+        }
+
+        for (let i = 0; i < 4; i++) {
+            const weekStart = new Date(startDate);
+            weekStart.setDate(weekStart.getDate() + i * 7);
+            const weekEnd = new Date(weekStart);
+            weekEnd.setDate(weekEnd.getDate() + 6);
+            const commandsForWeek = orderSucess.filter((command) => {
+              const commandDate = new Date(command.dateCommande);
+              return commandDate >= weekStart && commandDate <= weekEnd;
+            });
+          
+            const itemCount = commandsForWeek.length;
+            itemCountsByWeekfailed.push(itemCount);
+          }
+        new SuccessResponse("sucess",{statistic1 : {address , taux} ,
+         statistic2_Month : {success : orderSucessMonth , failed : orderFailedMonth} ,
+         statistic2_Week : {success : itemCountsByWeekSucess , failed : itemCountsByWeekfailed} ,
+          statistic2_Year : {success : { year : yearsSucess , count :  countByYearSucess} , failed : {year : yearsFailed , count : countByYearFailed}}   }).send(res);
                 
     })
